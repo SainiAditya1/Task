1a20f562e333e515c03c07bba67a6b1dcde4b800
Aditya Saini <adityasaini10012001@outlook.com>
2025-03-30 14:31:00 +0530

    Testing workflow

--- Task3/main.go
+++ Task3/main.go
@@ -10,7 +10,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	currentTime := time.Now().Format("2025-03-30 15:04:05")
 	fmt.Fprintf(w, "Current Date & Time: %s", currentTime)
 }
-
 func main() {
 	http.HandleFunc("/", handler)
 	fmt.Println("Server started on port 8080")
